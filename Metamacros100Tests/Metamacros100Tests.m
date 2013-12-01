80e25a69dece546345f20ca19e951b7264181e28
wanderwaltz <wanderwaltz@gmail.com>
2013-12-01 18:45:58 +0700

    Added tests for ww_metamacro_at and metamacro_at.

--- Metamacros100Tests/Metamacros100Tests.m
+++ Metamacros100Tests/Metamacros100Tests.m
@@ -196,5 +196,65 @@
 }
 
 
+#pragma mark -
+#pragma mark metamacro_at tests
+
+/// This case tests the ww_metamacro_at's core functionality
+- (void) test_ww_metamacro_at
+{
+    int x = ww_metamacro_at(1,
+                            1, 2, 3); // select arg with index 1 from arglist 1,2,3 -> should expand to 2.
+    
+    XCTAssertEqual(x, 2, @"ww_metamacto_at expands to the Nth argument of the args list starting from 0");
+    
+    int a100 = 12345;
+    
+    // The whole thing with the a0..a100 is introduced here just to make
+    // sure we can actually work with other things than just numbers.
+    int y = ww_metamacro_at(100,
+                             a0,
+                             a1,  a2,  a3,  a4,  a5,  a6,  a7,  a8,  a9,  a10,
+                            a11, a12, a13, a14, a15, a16, a17, a18, a19,  a20,
+                            a21, a22, a23, a24, a25, a26, ---, a28, a29,  a30,
+                            a31, ###, a33, a34, a35, a36, a37, a38, a39,  a40,
+                            a41, a42, a43, $$$, a45, a46, a47, a48, a49,  a50,
+                            a51, a52, a53, a54, a55, a56, a57, a58, a59,  a60,
+                            a61, a62, a63, a64, a65, a66, a67, !!!, a69,  a70,
+                            a71, a72, a73, a74, a75, a76, a77, a78, a79,  a80,
+                            a81, a82, a83, a84, a85, a86, a87, a88, a89,  ***,
+                            a91, a92, a93, a94, a95, a96, a97, a98, a99, a100);
+    
+    XCTAssertEqual(y, a100, @"ww_metamacro_at supports up to 100 arguments.");
+}
+
+
+/// This case tests that ww_metamacro_at has been properly aliased to the metamacro_at
+- (void) test_metamacro_at
+{
+    int x = metamacro_at(1,
+                         1, 2, 3); // select arg with index 1 from arglist 1,2,3 -> should expand to 2.
+    
+    XCTAssertEqual(x, 2, @"metamacto_at expands to the Nth argument of the args list starting from 0");
+    
+    int a100 = 12345;
+    
+    // The whole thing with the a0..a100 is introduced here just to make
+    // sure we can actually work with other things than just numbers.
+    int y = metamacro_at(100,
+                         a0,
+                         a1,  a2,  a3,  a4,  a5,  a6,  a7,  a8,  a9,  a10,
+                         a11, a12, a13, a14, a15, a16, a17, a18, a19,  a20,
+                         a21, a22, a23, a24, a25, a26, ---, a28, a29,  a30,
+                         a31, ###, a33, a34, a35, a36, a37, a38, a39,  a40,
+                         a41, a42, a43, $$$, a45, a46, a47, a48, a49,  a50,
+                         a51, a52, a53, a54, a55, a56, a57, a58, a59,  a60,
+                         a61, a62, a63, a64, a65, a66, a67, !!!, a69,  a70,
+                         a71, a72, a73, a74, a75, a76, a77, a78, a79,  a80,
+                         a81, a82, a83, a84, a85, a86, a87, a88, a89,  ***,
+                         a91, a92, a93, a94, a95, a96, a97, a98, a99, a100);
+    
+    XCTAssertEqual(y, a100, @"metamacro_at supports up to 100 arguments.");
+}
+
 
 @end
